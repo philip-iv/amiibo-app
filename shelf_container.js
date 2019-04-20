c4449db78f87546f9815980a57545a021a4402af
Philip Stephenson <philipstephenson4@gmail.com>
2019-04-19 22:55:23 -0400

    Add unique keys to react objects

--- shelf_container.js
+++ shelf_container.js
@@ -4,25 +4,26 @@ const e = React.createElement;
 
 var characters = ['Alm', 'Bayonetta', 'Boo', 'Bowser Jr', 'Bowser', 'Captain Falcon', 'Celica', 'Charizard', 'Chibi Robo', 'Chrom', 'Cloud', 'Corrin (Female)', 'Corrin (Male)', 'Daisy (Super Mario)', 'Daisy', 'Dark Pit', 'Dark Samus', 'Diddy Kong', 'Donkey Kong', 'Dr Mario', 'Duck Hunt', 'Falco', 'Fox', 'Game and Watch', 'Ganondorf', 'Goomba', 'Greninja', 'Ice Climbers', 'Ike', 'Inkling', 'Ivysaur', 'Jigglypuff', 'King Dedede', 'King K Rool', 'kirby-WaddleDee', 'Kirby', 'Link (Adult)', 'Link (BOTW)', 'Little Mac', 'Lucario', 'Lucas', 'Lucina', 'Luigi', 'Mario (Gold)', 'Mario (Silver)', 'Mario', 'Marth', 'Mega Man', 'Meta Knight', 'Mewtwo', 'Mii Brawler', 'Mii Gunner', 'Mii Swordfighter', 'Ness', 'Olimar', 'Pac-Man', 'Palutena', 'Peach', 'Pichu', 'PIK-pikmin', 'Pikachu', 'Pit', 'Pokemon Trainer', 'Richter', 'Ridley', 'ROB', 'Robin (Male)', 'Rosalina', 'Roy', 'Ryu', 'Samus', 'Sheik', 'Shulk', 'Simon', 'SM-koopa', 'Snake', 'Sonic', 'splat-boy-2', 'splat-boy-violet', 'splat-boy', 'splat-girl-2', 'splat-girl-green', 'splat-girl', 'splat-green-ENG', 'splat-pink-ENG', 'splat-squid-2', 'splat-squid-oran', 'splat-squid', 'Squirtle', 'Toad', 'Toon Link', 'Villager', 'Waluigi', 'Wario', 'Wii Fit Trainer', 'Wolf', 'Yoshi', 'Young Link', 'YWW-blu', 'YWW-green', 'YWW-pink', 'YWW-poochy', 'Z-botw-daruk', 'Z-botw-mipha', 'Z-botw-revali', 'Z-botw-urbosa', 'Zelda-SSBU', 'Zelda', 'Zero Suit Samus']
 var char_options = []
-for (let c of characters) {
-    char_options.push(e('option', {value: c}, c));
+for (var i = 0; i < characters.length; i++) {
+    var c = characters[i];
+    char_options.push(e('option', {value: c, key: i}, c));
 }
 
-function tr(content) {
-    return e('tr', [], content)
+function tr(content, params) {
+    return e('tr', params, content)
 }
-function td(content) {
-    return e('td', [], content)
+function td(content, params) {
+    return e('td', params, content)
 }
 
-function table(content) {
-    return e('table', [], e('tbody', [], content));
+function table(content, t_params, tb_params) {
+    return e('table', t_params, e('tbody', tb_params, content));
 }
 
 function make_rows(items) {
     var rows = [];
-    for (let r of items) {
-        rows.push(tr(td(r)));
+    for (var i = 0; i < items.length; i++) {
+        rows.push(tr(td(items[i], {key: "td"+i}), {key: "tr"+i}));
     }
     return rows;
 }
@@ -40,9 +41,9 @@ class Amiibo extends React.Component {
 
     render() {
         var img_name = "images/" + this.state.character + ".png";
-        var img = e('img', {src: img_name, height: 200});
-        var selector = e('select', {onChange: this.handleChange}, char_options);
-        var container = table(make_rows([img, selector]));
+        var img = e('img', {src: img_name, height: 200, key: "img"});
+        var selector = e('select', {onChange: this.handleChange, key: "char_selector"}, char_options);
+        var container = table(make_rows([img, selector]),{key: "atable"},{key: "atbody"});
         return container;
     }
 }
@@ -50,19 +51,20 @@ class Amiibo extends React.Component {
 class ShelfBase extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {color: "#333333"};
+        this.state = {color: "#000000"};
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
+        console.log(event);
         this.setState({color: event.target.value});
     }
 
     render() {
         var styling = {height: "30px", backgroundColor: this.state.color};
-        var row_props = {style: styling, colSpan: 4};
+        var row_props = {style: styling, colSpan: 4, key: "base-td"};
         var row = e('td', row_props);
-        var input = td(e('input', {type: "text", value: this.state.color, onChange: this.handleChange}));
+        var input = td(e('input', {type: "text", value: this.state.color, onChange: this.handleChange, key: "color-picker"}), {key: "colortd"});
         return [row, input];
     }
 }
@@ -70,33 +72,35 @@ class ShelfBase extends React.Component {
 class Shelf extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {amiibo: [e(Amiibo),e(Amiibo),e(Amiibo),e(Amiibo)],
-                        base: e(ShelfBase)};
+        var amiibo = [0,1,2,3].map((n) => e(Amiibo, {key: "amiibo-"+n}));
+        this.state = {amiibo: amiibo,
+                        base: e(ShelfBase, {key: "shelfbase"})};
     }
 
     render() {
         var am = [];
-        for (let a of this.state.amiibo) {
-            am.push(td(a));
+        for (var i = 0; i < this.state.amiibo.length; i++) {
+            am.push(td(this.state.amiibo[i], {key: i}));
         }
-        return table([tr(am), tr(this.state.base)]);
+        return table([tr(am, {key: "amiibo"}), tr(this.state.base, {key: "base"})], {key: "tb"}, {key: "tbod"});
     }
 }
 
 class ShelfContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {shelves: [e(Shelf),e(Shelf),e(Shelf),e(Shelf)]};
+        var shelves = [0,1,2,3].map((n) => e(Shelf, {key: n}));
+        this.state = {shelves: shelves};
     }
 
     render() {
         var sh = [];
-        for (let s of this.state.shelves) {
-            sh.push(tr(td(s)));
+        for (var i = 0; i < this.state.shelves.length; i++) {
+            sh.push(tr(td(this.state.shelves[i], {key: "td"+i}), {key: "tr"+i}));
         }
-        return table(sh);
+        return table(sh, {key: "table"}, {key: "tbody"});
     }
 }
 
 const domContainer = document.querySelector('#shelf_container');
-ReactDOM.render(e(ShelfContainer), domContainer);
+ReactDOM.render(e(ShelfContainer, {key: "container"}), domContainer);
